f8cea2b283cdd8e249c798e444c390528b2ac531
Sodiqmirzo <sattorov.s@corp.click.uz>
2025-04-03 16:21:47 +0500

    first commit

---
+++ src/components/AppMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-toolbar color="primary">
+    <v-spacer />
+    <v-btn
+      @click="$emit('show-qr-scanner')"
+    >
+      <v-icon>
+        mdi-qrcode-scan
+      </v-icon>
+      Scan
+    </v-btn>
+    <v-spacer />
+    <v-btn
+      @click="$emit('show-history')"
+    >
+      <v-icon>
+        mdi-history
+      </v-icon>
+      History
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: "AppMenu",
+  emits: [
+    'show-qr-scanner',
+    'show-history',
+    'show-settings'
+  ],
+});
+</script>
